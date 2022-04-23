7ea45978d6a6ef98771f936453036b744b7817f5
Howard <howard.huang@mail.mcgill.ca>
2022-04-22 20:23:33 -0400

    Orion

--- setup.py
+++ setup.py
@@ -6,4 +6,10 @@ setuptools.setup(
     version="0.0.13",
     author="TorchBeast team",
     install_requires=[],
+    entry_points={
+        "BaseExploit": [
+            "multitasktruncateexploit = torchbeast.pbt_exploit:MultitaskTruncateExploit",
+            "multitaskbacktrackexploit = torchbeast.pbt_exploit:MultitaskBacktrackExploit",
+        ]
+    },
 )
